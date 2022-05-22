b8f9873fd784fcfbcb2ca8be2abfa0aa5bbe27b8
Ravindra <ravindrareddy1217@gmail.com>
2022-05-22 07:18:42 +0530

    updated

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 export default function Leaderboard(props) {
   return(  <>
     <p className="text-center text-md font-regular mr-1 width50100 text-xl">
+
       
     </p>
   </>)
